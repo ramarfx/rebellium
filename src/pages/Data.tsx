e28abe0b0ea6495eb971a953fe2f40f56816b77f
ramarfx <ramagamiing33@gmail.com>
2024-07-06 23:11:02 +0700

    fix: change dataset chart

--- src/pages/Data.tsx
+++ src/pages/Data.tsx
@@ -1,34 +1,46 @@
-import { CategoryScale, Chart, LinearScale, LineController, LineElement, PointElement, Title } from "chart.js"
-import { useEffect, useRef } from "react"
+import {
+  CategoryScale,
+  Chart,
+  LinearScale,
+  LineController,
+  LineElement,
+  PointElement,
+  Title,
+} from "chart.js";
+import { useEffect, useRef } from "react";
+import { dataButtonList } from "../docs/dataButton";
+import { data } from "../docs/dataChart";
 
-Chart.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale);
+Chart.register(
+  LineController,
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+  CategoryScale
+);
 
 export const Data = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const chartRef = useRef<Chart | null>(null);
+  const buttonRefs = useRef<HTMLButtonElement[]>([]);
 
-  const data = [
-    { year: "2019", kasus: 0.4 },
-    { year: "2020", kasus: 0.65 },
-    { year: "2021", kasus: 0.22 },
-    { year: "2022", kasus: 0.14 },
-    { year: "2023", kasus: 0.16 },
-  
-  ];
+  let currentData = data[0];
 
   useEffect(() => {
-    if (canvasRef.current) {
+    if (canvasRef.current && buttonRefs.current) {
       if (chartRef.current) {
         chartRef.current.destroy();
       }
+
       chartRef.current = new Chart(canvasRef.current, {
         type: "line",
         data: {
-          labels: data.map(item => item.year),
+          labels: data[0].map((item) => item.year),
           datasets: [
             {
               label: "kasus (%)",
-              data: data.map(item => item.kasus),
+              data: currentData.map((item) => item.kasus),
               pointStyle: "circle",
               borderWidth: 3,
               pointRadius: 10,
@@ -36,13 +48,30 @@ export const Data = () => {
               borderColor: "#6171C8",
               fill: true,
               backgroundColor: "#F89B11",
-            }
-          ]
-        }
-      })
+            },
+          ],
+        },
+      });
+
+      buttonRefs.current.forEach((button, index) => {
+        button.onclick = () => {
+          // eslint-disable-next-line react-hooks/exhaustive-deps
+          currentData = data[index];
+          
+          // Update chart data
+          if (chartRef.current) {
+            chartRef.current.data.labels = currentData.map((item) => item.year);
+            chartRef.current.data.datasets[0].data = currentData.map(
+              (item) => item.kasus
+            );
+            chartRef.current.update();
+          }
+
+        };
+      });
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <section id="data" className="pb-[600px] md:pb-[1000px]">
@@ -59,17 +88,12 @@ export const Data = () => {
         data-aos-easing="ease-out"
         data-aos-duration="300"
         id="data-children"
-        className="z-20 -mt-20 h-96 w-full rounded-t-3xl bg-white pt-10 md:-mt-28 md:rounded-t-[126px]"
-      >
+        className="z-20 -mt-20 h-96 w-full rounded-t-3xl bg-white pt-10 md:-mt-28 md:rounded-t-[126px]">
         <div id="data-inner" className="container">
-          <h1
-            className="mb-10 text-center font-poppins text-3xl font-bold md:text-4xl"
-          >
+          <h1 className="mb-10 text-center font-poppins text-3xl font-bold md:text-4xl">
             Kumpulan Data <span className="text-yellow">Kenakalan Remaja </span>
           </h1>
-          <p
-            className="mx-auto max-w-3xl text-center text-sm text-secondary-200 md:text-base"
-          >
+          <p className="mx-auto max-w-3xl text-center text-sm text-secondary-200 md:text-base">
             Dengan data kenakalan remaja kita dapat mencerminkan perilaku
             negatif seseorang, seperti remaja yang menggunakan narkoba, tawuran,
             merokok, dan kenakalan remaja lainnya. Dengan demikian kita dapat
@@ -79,150 +103,47 @@ export const Data = () => {
 
           <div
             id="button-container"
-            className="mt-10 grid grid-cols-2 grid-rows-3 flex-wrap items-center justify-center gap-5 md:flex"
-          >
+            className="mt-10 grid grid-cols-2 grid-rows-3 flex-wrap items-center justify-center gap-5 md:flex">
             {/* <!-- card 1 --> */}
-            <button
-              data-aos-delay="100"
-              id="tawuran"
-              className="flex w-full max-w-xs cursor-pointer gap-2 rounded-xl bg-[#6874B9] p-3 transition hover:-translate-y-1 hover:bg-yellow hover:opacity-90 md:w-fit"
-            >
-              <div
-                className="flex h-[48px] w-[80px] items-center justify-center rounded-lg bg-white text-black md:h-[96px] md:w-[96px]"
-              >
-                <img
-                  src="/icon/celurit.png"
-                  className="h-auto w-full"
-                  alt=""
-                />
-              </div>
+            {dataButtonList.map((button, index) => (
+              <button
+                key={index}
+                ref={(e) =>
+                  (buttonRefs.current[index] = e as HTMLButtonElement)
+                }
+                className="flex w-full max-w-xs cursor-pointer gap-2 rounded-xl bg-[#6874B9] p-3 transition hover:-translate-y-1 hover:bg-yellow hover:opacity-90 md:w-fit">
+                <div className="flex h-[48px] w-[80px] items-center justify-center rounded-lg bg-white text-black md:h-[96px] md:w-[96px]">
+                  <img src={button.image} className="h-auto w-full" alt="" />
+                </div>
 
-              <div className="text-start text-white">
-                <h5 className="text-xs font-bold md:text-xl">Tawuran</h5>
-                <p className="text-xs md:text-lg">Berada di angka 16%</p>
-              </div>
-            </button>
-            {/* <!-- card 2 --> */}
-            <button
-              id="narkoba"
-              className="flex w-full max-w-xs cursor-pointer gap-2 rounded-xl bg-[#6874B9] p-3 transition hover:-translate-y-1 hover:bg-yellow hover:opacity-90 md:w-fit"
-            >
-              <div
-                className="flex h-[48px] w-[80px] items-center justify-center rounded-lg bg-white text-black md:h-[96px] md:w-[96px]"
-              >
-                <img
-                  src="/icon/narkoba.png"
-                  className="h-auto w-full"
-                  alt=""
-                />
-              </div>
-
-              <div className="text-left text-white">
-                <h5 className="text-xs font-bold md:text-xl">Narkoba</h5>
-                <p className="text-xs md:text-lg">Berada di angka 16%</p>
-              </div>
-            </button>
-            {/* <!-- card 3 --> */}
-            <button
-              id="merokok"
-              className="flex w-full max-w-xs cursor-pointer gap-2 rounded-xl bg-[#6874B9] p-3 transition hover:-translate-y-1 hover:bg-yellow hover:opacity-90 md:w-fit"
-            >
-              <div
-                className="flex h-[48px] w-[80px] items-center justify-center rounded-lg bg-white text-black md:h-[96px] md:w-[96px]"
-              >
-                <img
-                  src="/icon/rokok.png"
-                  className="h-auto w-full"
-                  alt=""
-                />
-              </div>
-
-              <div className="text-left text-white">
-                <h5 className="text-xs font-bold md:text-xl">Merokok</h5>
-                <p className="text-xs md:text-lg">Berada di angka 18.8%</p>
-              </div>
-            </button>
-            {/* <!-- card 4 --> */}
-            <button
-              id="seksual"
-              className="flex w-full max-w-xs cursor-pointer gap-2 rounded-xl bg-[#6874B9] p-3 transition hover:-translate-y-1 hover:bg-yellow hover:opacity-90 md:w-fit"
-            >
-              <div
-                className="flex h-[48px] w-[80px] items-center justify-center rounded-lg bg-white text-black md:h-[96px] md:w-[96px]"
-              >
-                <img
-                  src="/icon/seksual.png"
-                  className="h-auto w-full"
-                  alt=""
-                />
-              </div>
-
-              <div className="text-left text-white">
-                <h5 className="text-xs font-bold md:text-xl">Seksual</h5>
-                <p className="text-xs md:text-lg">Mencapai hingga 21,4%</p>
-              </div>
-            </button>
-            {/* <!-- card 5 --> */}
-            <button
-              id="perundungan"
-              className="flex w-full max-w-xs cursor-pointer gap-2 rounded-xl bg-[#6874B9] p-3 transition hover:-translate-y-1 hover:bg-yellow hover:opacity-90 md:w-fit"
-            >
-              <div
-                className="flex h-[48px] w-[80px] items-center justify-center rounded-lg bg-white text-black md:h-[96px] md:w-[96px]"
-              >
-                <img
-                  src="/icon/bully.png"
-                  className="h-auto w-full"
-                  alt=""
-                />
-              </div>
-
-              <div className="text-left text-white">
-                <h5 className="text-xs font-bold md:text-xl">Perundungan</h5>
-                <p className="text-xs md:text-lg">Berada di angka 13.5%</p>
-              </div>
-            </button>
-            {/* <!-- card 6 --> */}
-            <button
-              id="minumanKeras"
-              className="flex w-full max-w-xs cursor-pointer gap-2 rounded-xl bg-[#6874B9] p-3 transition hover:-translate-y-1 hover:bg-yellow hover:opacity-90 md:w-fit"
-            >
-              <div
-                className="flex h-[48px] w-[80px] items-center justify-center rounded-lg bg-white text-black md:h-[96px] md:w-[96px]"
-              >
-                <img
-                  src="/icon/miras.png"
-                  className="h-auto w-full"
-                  alt=""
-                />
-              </div>
-
-              <div className="text-left text-white">
-                <h5 className="text-xs font-bold md:text-xl">Mabuk</h5>
-                <p className="text-xs md:text-lg">Berada di angka 1.4%</p>
-              </div>
-            </button>
+                <div className="text-start text-white">
+                  <h5 className="text-xs font-bold md:text-xl">
+                    {button.label}
+                  </h5>
+                  <p className="text-xs md:text-lg">{button.description}</p>
+                </div>
+              </button>
+            ))}
           </div>
 
           <h1
             data-aos="fade-right"
             data-aos-offset="200"
-            className="my-10 font-poppins text-2xl font-bold md:ml-20"
-          >
-            Persentase Data dalam <span className="text-yellow">Bentuk Grafik</span>
+            className="my-10 font-poppins text-2xl font-bold md:ml-20">
+            Persentase Data dalam{" "}
+            <span className="text-yellow">Bentuk Grafik</span>
           </h1>
 
           <div className="w-full">
             <canvas
               ref={canvasRef}
               id="myChart"
-              className="my-5 mb-[600px] h-max border md:container md:mx-20 md:py-5"
-            ></canvas>
+              className="my-5 mb-[600px] h-max border md:container md:mx-20 md:py-5"></canvas>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Data
+export default Data;
