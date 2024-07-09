fc7d50a9e7297d0023d66c7e31082ecb4a4fada6
ramarfx <ramagamiing33@gmail.com>
2024-07-09 23:35:35 +0700

    add: environment graphic

--- src/components/Sabit.tsx
+++ src/components/Sabit.tsx
@@ -1,7 +1,7 @@
-import { Canvas, extend } from "@react-three/fiber";
+import { Canvas, extend} from "@react-three/fiber";
 import { Suspense } from "react";
 import Loader from "./Loader";
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
 import Celurit from "./models/Celurit";
 
 extend({
@@ -10,19 +10,34 @@ extend({
 });
 
 const Sabit = () => {
+
   return (
     <Canvas
       className="h-[350px] w-screen md:h-[300px] md:w-[500px] border"
       camera={{ position: [0, 0, 82] }}>
       <Suspense fallback={<Loader />}>
-        <ambientLight intensity={8} />
         <OrbitControls enablePan={false} />
 
+        <ambientLight intensity={8} />
+
         <group position={[0, 25, 0]}>
-          <Celurit scale={25} position={[-20, 0, 0]} rotation={[0, 0, Math.PI / 6]}  />
-          <Celurit scale={25} position={[20, 0, 0]} rotation={[0, Math.PI, Math.PI / 6]}  />
+          <directionalLight intensity={5} position={[30, 50, 30]} />
+          <directionalLight intensity={5} position={[-30, -15, -30]} />
+
+          <Celurit
+            scale={25}
+            position={[-20, 0, 0]}
+            rotation={[0, 0, Math.PI / 6]}
+          />
+          <Celurit
+            scale={25}
+            position={[20, 0, 0]}
+            rotation={[0, Math.PI, Math.PI / 6]}
+          />
         </group>
+
       </Suspense>
+      <Environment preset="sunset" />
     </Canvas>
   );
 };
