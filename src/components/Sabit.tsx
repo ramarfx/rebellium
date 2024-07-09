8ac59d5b41070809a6c0ab5dffff3af9df9148d5
ramarfx <ramagamiing33@gmail.com>
2024-07-10 00:05:57 +0700

    add: animation rotation

--- src/components/Sabit.tsx
+++ src/components/Sabit.tsx
@@ -1,43 +1,53 @@
-import { Canvas, extend} from "@react-three/fiber";
-import { Suspense } from "react";
+import { Canvas, extend } from "@react-three/fiber";
+import { Suspense, useRef } from "react";
 import Loader from "./Loader";
 import { Environment, OrbitControls } from "@react-three/drei";
 import Celurit from "./models/Celurit";
+import { useRotateAnimation } from "../func/rotateAnimation";
+import { Group } from "three";
 
 extend({
   OrbitControls,
   Celurit,
 });
 
-const Sabit = () => {
+const Scene = () => {
+  const celuritRef = useRef<Group>(null!);
+  useRotateAnimation(celuritRef);
 
   return (
+    <>
+      <OrbitControls enablePan={false} enableZoom={false} />
+      <ambientLight intensity={8} />
+
+      <group position={[0, 20, 0]} ref={celuritRef}>
+        <directionalLight intensity={5} position={[30, 50, 30]} />
+        <directionalLight intensity={5} position={[-30, -15, -30]} />
+
+        <Celurit
+          scale={25}
+          position={[-20, 0, 0]}
+          rotation={[0, 0, Math.PI / 6]}
+        />
+        <Celurit
+          scale={25}
+          position={[20, 0, 0]}
+          rotation={[0, Math.PI, Math.PI / 6]}
+        />
+      </group>
+    </>
+  );
+};
+
+const Sabit = () => {
+  return (
     <Canvas
-      className="h-[350px] w-screen md:h-[300px] md:w-[500px] border"
+      className="h-[350px] w-screen md:h-[300px] md:w-[500px]"
       camera={{ position: [0, 0, 82] }}>
       <Suspense fallback={<Loader />}>
-        <OrbitControls enablePan={false} />
-
-        <ambientLight intensity={8} />
-
-        <group position={[0, 25, 0]}>
-          <directionalLight intensity={5} position={[30, 50, 30]} />
-          <directionalLight intensity={5} position={[-30, -15, -30]} />
-
-          <Celurit
-            scale={25}
-            position={[-20, 0, 0]}
-            rotation={[0, 0, Math.PI / 6]}
-          />
-          <Celurit
-            scale={25}
-            position={[20, 0, 0]}
-            rotation={[0, Math.PI, Math.PI / 6]}
-          />
-        </group>
-
+        <Scene />
+        <Environment preset="sunset" />
       </Suspense>
-      <Environment preset="sunset" />
     </Canvas>
   );
 };
