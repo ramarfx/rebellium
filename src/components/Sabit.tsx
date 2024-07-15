1bda1503c65793b851c68d75e285ad008d1635de
ramarfx <ramagamiing33@gmail.com>
2024-07-15 21:23:08 +0700

    finish: sabit 3D dan kawan kawan

--- src/components/Sabit.tsx
+++ src/components/Sabit.tsx
@@ -8,6 +8,7 @@ import { Group } from "three";
 import Kapsul from "./models/Kapsul";
 import Botol from "./models/Botol";
 import Obat from "./models/Obat";
+import { useFloatAnimation } from "../func/floatAnimation";
 
 extend({
   OrbitControls,
@@ -17,8 +18,20 @@ extend({
 
 const Scene = () => {
   const rotateGroupRef = useRef<Group>(null!);
-  const floatGroupRef = useRef<Group>(null!);
+  // const floatGroupRef = useRef<Group>(null!);
+  const kapsul1Ref = useRef<Group>(null!);
+  const kapsul2Ref = useRef<Group>(null!);
+  const botol1Ref = useRef<Group>(null!);
+  const botol2Ref = useRef<Group>(null!);
+  const obatRef = useRef<Group>(null!);
+
   useRotateAnimation(rotateGroupRef);
+
+  useFloatAnimation(kapsul1Ref, 1000);
+  useFloatAnimation(kapsul2Ref, 1100);
+  useFloatAnimation(botol1Ref, 500);
+  useFloatAnimation(botol2Ref, 700);
+  useFloatAnimation(obatRef, 900);
 
   return (
     <>
@@ -29,7 +42,7 @@ const Scene = () => {
         <directionalLight intensity={5} position={[30, 50, 30]} />
         <directionalLight intensity={5} position={[-30, -15, -30]} />
 
-        <group ref={rotateGroupRef}>
+        <group>
           <Celurit
             scale={25}
             position={[-20, 0, 0]}
@@ -42,28 +55,42 @@ const Scene = () => {
           />
         </group>
 
-        <group ref={floatGroupRef}>
-          <Kapsul
-            scale={5}
-            position={[20, 0, -10]}
-            rotation={[0, 0, Math.PI / 8]}
-          />
-          <Kapsul
-            scale={5}
-            position={[-35, -25, 10]}
-            rotation={[0, 0, Math.PI / -4]}
-          />
-          <Botol
-            scale={30}
-            position={[-25, 0, -5]}
-            rotation={[0, 0, Math.PI / 8]}
-          />
-          <Botol
-            scale={30}
-            position={[45, 10, 10]}
-            rotation={[0, 0, Math.PI / -6]}
-          />
-          <Obat scale={10} position={[40, -30, -5]} rotation={[0, Math.PI / 2, Math.PI / 4]} />
+        <group position={[0, -20, 0]}>
+          <group ref={kapsul1Ref}>
+            <Kapsul
+              scale={5}
+              position={[20, 0, -10]}
+              rotation={[0, 0, Math.PI / 8]}
+            />
+          </group>
+          <group ref={kapsul2Ref}>
+            <Kapsul
+              scale={5}
+              position={[-35, -25, 10]}
+              rotation={[0, 0, Math.PI / -4]}
+            />
+          </group>
+          <group ref={botol1Ref}>
+            <Botol
+              scale={30}
+              position={[-25, 0, -5]}
+              rotation={[0, 0, Math.PI / 8]}
+            />
+          </group>
+          <group ref={botol2Ref}>
+            <Botol
+              scale={30}
+              position={[45, 10, 10]}
+              rotation={[0, 0, Math.PI / -6]}
+            />
+          </group>
+          <group ref={obatRef}>
+            <Obat
+              scale={10}
+              position={[40, -30, -5]}
+              rotation={[0, Math.PI / 2, Math.PI / 4]}
+            />
+          </group>
         </group>
       </group>
     </>
